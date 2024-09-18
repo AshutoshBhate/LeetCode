5586bea3f0f13c9c0f52ed90b01843cd7eca7587
CeLViron <ashutoshbhate7@gmail.com>
2024-09-18 17:14:46 +0530

    Made Changes

--- 11_ContainerWithMostWater.cpp
+++ 11_ContainerWithMostWater.cpp
@@ -1,31 +1,34 @@
-#include <cmath>
-
 class Solution {
 public:
     int maxArea(vector<int>& height)
     {
-        map<int, int> height_adjust;
-        map<int, int> index_adjust;
-        map<int, int> :: iterator iter;
+        multimap<int, int> height_adjust;
+        multimap<int, int> index_adjust;
+        multimap<int, int> :: iterator iter;
         long multiplication = 0, super_multiplication = 0;
 
         for(int i = 0; i < height.size(); i++)
         {
-            height_adjust[height[i]] = i;
-            index_adjust[i] = height[i];
+            height_adjust.emplace(height[i], i);
+            index_adjust.emplace(i, height[i]);
         }
         for(iter = height_adjust.begin(); iter != height_adjust.end(); iter++)
         {
-            if((*iter).second >int(abs((*iter).second - (index_adjust.size() - 1))))
+            if((*iter).second > static_cast<int>(abs((*iter).second - static_cast<int>((index_adjust.size() - 1)))))
             {
                 multiplication = (*iter).second * (*iter).first;
 
             }
             else
             {
-                multiplication = abs((*iter).second - (index_adjust.size() - 1)) * (*iter).first;
+                multiplication = static_cast<long>(abs((*iter).second - static_cast<long>((index_adjust.size() - 1)))) * (*iter).first;
             }
-            index_adjust.erase(*iter.second);
+            index_adjust.erase((*iter).second);
+
+            // auto it = index_adjust.find((*iter).second);
+            // if (it != index_adjust.end()) {
+            //     index_adjust.erase(it);
+            // }
 
             if( multiplication > super_multiplication)
             {
