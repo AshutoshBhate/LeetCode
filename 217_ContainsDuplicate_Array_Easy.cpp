61c00b0f6c1529daf7530563f2daef885375feeb
CeLViron <ashutoshbhate7@gmail.com>
2024-09-24 17:27:57 +0530

    Made Changes

---
+++ 217_ContainsDuplicate_Array_Easy.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    bool containsDuplicate(vector<int>& nums)
+    {
+        cin.tie(nullptr);
+        cout.tie(nullptr);
+        ios::sync_with_stdio(false);
+        sort(nums.begin(),nums.end());
+        for(int i=1;i<nums.size();i++){
+            if(nums[i]==nums[i-1]){
+                return true;
+            }
+        }
+        return false;
+    }
+};
