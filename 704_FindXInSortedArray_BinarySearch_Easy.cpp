eec0ae4c5440d64d26afec7ef7139c518703b0ac
CeLViron <ashutoshbhate7@gmail.com>
2024-09-27 02:30:15 +0530

    Made Changes

---
+++ 704_FindXInSortedArray_BinarySearch_Easy.cpp
@@ -0,0 +1,37 @@
+class Solution {
+public:
+    int search(vector<int>& nums, int target)
+    {
+        cin.tie(nullptr);
+        cout.tie(nullptr);
+        ios::sync_with_stdio(0);
+
+        int left = 0, right = nums.size() - 1;
+        int middle;
+
+        while(left < right)
+        {
+            middle = (left + right)/2;
+            if(nums[middle] == target)
+            {
+                return middle;
+            }
+            if(nums[middle] > target)
+            {
+                right = middle - 1;
+            }
+            else
+            {
+                left = middle + 1;
+            }
+        }
+        if(left == right && nums[right] == target)
+        {
+            return right;
+        }
+        else
+        {
+            return -1;
+        }
+    }
+};
