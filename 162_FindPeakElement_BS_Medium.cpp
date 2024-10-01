cde7b9e69813c5e81067ba3b87ca5cdb342f97fd
CeLViron <ashutoshbhate7@gmail.com>
2024-10-01 17:19:22 +0530

    Made Changes

---
+++ 162_FindPeakElement_BS_Medium.cpp
@@ -0,0 +1,67 @@
+//Brute Force
+
+class Solution {
+public:
+    int findPeakElement(vector<int>& nums)
+    {
+        cin.tie(nullptr);
+        cout.tie(nullptr);
+        ios::sync_with_stdio(false);
+
+        int i;
+
+        for(i = 0; i < nums.size(); i ++)
+        {
+            if((i == 0 || nums[i - 1] < nums[i]) && i == nums.size() - 1 || nums[i + 1] < nums[i])
+            {
+                return i;
+            }
+        }
+
+        return i;
+    }
+};
+
+//Better
+
+class Solution {
+public:
+    int findPeakElement(vector<int>& nums)
+    {
+        cin.tie(nullptr);
+        cout.tie(nullptr);
+        ios::sync_with_stdio(false);
+
+        int left = 0, right = nums.size() - 1, mid;
+
+        while(left <= right)
+        {
+            mid =  (left + right)/2;
+            if(mid == 0 || mid == nums.size() - 1)
+            {
+                auto maxElement = max_element(nums.begin(), nums.end());
+                return distance(nums.begin(), maxElement);
+            }
+            else
+            {
+                if(nums[mid - 1] < nums[mid] && nums[mid + 1] < nums[mid])
+                {
+                    return mid;
+                }
+                else
+                {
+                    if(nums[mid - 1] > nums[mid])
+                    {
+                        right  = mid - 1;
+                    }
+                    else
+                    {
+                        left = mid + 1;
+                    }
+                }
+            }
+        }
+        return mid;
+    }
+};
+
