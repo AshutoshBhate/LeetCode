c8ae9158a4194d5d1911e7a1f6c088772fcaf2a4
CeLViron <ashutoshbhate7@gmail.com>
2024-10-01 19:25:46 +0530

    Made Changes

--- 162_FindPeakElement_BS_Medium.cpp
+++ 162_FindPeakElement_BS_Medium.cpp
@@ -65,3 +65,53 @@ public:
     }
 };
 
+//Optimal
+
+class Solution{
+public:
+    int findPeakElement(vector<int>& nums)
+    {
+        cin.tie(nullptr);
+        cout.tie(nullptr);
+        ios::sync_with_stdio(false);
+
+        int n = nums.size();
+        if (n == 1)
+        {
+            return 0;
+        }
+        if (nums[0] > nums[1])
+        {
+            return 0;
+        }
+        if (nums[n - 1] > nums[n - 2])
+        {
+            return n - 1;
+        }
+
+        int low = 1, high = n - 2;
+
+        while (low <= high)
+        {
+            int mid = (low + high) / 2;
+
+            if (nums[mid - 1] < nums[mid] && nums[mid] > nums[mid + 1])
+            {
+                return mid;
+            }
+            if (nums[mid] > nums[mid - 1])
+            {
+                low = mid + 1;
+            }
+            else
+            {
+                high = mid - 1;
+            }
+        }
+        // Dummy return statement
+        return -1;
+    }
+};
+
+
+
