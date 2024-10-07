b1299f822c49a7047a5b905eeaa27053c0d4b8e3
CeLViron <ashutoshbhate7@gmail.com>
2024-10-07 23:33:39 +0530

    Made Changes

---
+++ GFG_SearchInLinkedList_LinkedList.cpp
@@ -0,0 +1,28 @@
+/* Link list node */
+/*
+struct Node
+{
+    int data;
+    Node* next;
+    Node(int x) {  data = x;  next = NULL; }
+}; */
+
+class Solution {
+  public:
+    // Function to count nodes of a linked list.
+    bool searchKey(int n, struct Node* head, int key)
+    {
+        while(head != NULL)
+        {
+            if(head -> data == key)
+            {
+                return true;
+            }
+            else
+            {
+                head = head -> next;
+            }
+        }
+        return false;
+    }
+};
