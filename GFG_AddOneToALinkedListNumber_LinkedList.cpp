1cc2b0c115d406ce4596936aa5e51b99636d9b3e
CeLViron <ashutoshbhate7@gmail.com>
2024-10-20 01:43:04 +0530

    Made Changes

---
+++ GFG_AddOneToALinkedListNumber_LinkedList.cpp
@@ -0,0 +1,51 @@
+/*
+
+struct Node
+{
+    int data;
+    struct Node* next;
+
+    Node(int x){
+        data = x;
+        next = NULL;
+    }
+};
+
+*/
+
+class Solution {
+  public:
+  
+    int helper(Node* temp)
+    {
+        if(temp == NULL)
+        {
+            return 1;
+        }
+        
+        int carry = helper(temp -> next);
+        temp -> data = temp -> data + carry;
+        if(temp -> data < 10)
+        {
+            return 0;
+        }
+        else
+        {
+            temp -> data = 0;
+            return 1;
+        }
+    }
+    
+    Node* addOne(Node* head)
+    {
+        int carry = helper(head);
+        if(carry == 1)
+        {
+            Node* newNode = new Node(1);
+            newNode -> next = head;
+            head = newNode;
+        }
+        
+        return head;
+    }
+};
---
+++ GFG_ArrayToDoublyLinkedList.cpp
@@ -0,0 +1,47 @@
+// User function Template for C++
+
+/*
+class Node{
+public:
+    Node* prev;
+    int data;
+    Node* next;
+
+    Node()
+    {
+        prev = NULL;
+        data = 0;
+        next = NULL;
+    }
+
+    Node(int value)
+    {
+        prev = NULL;
+        data = value;
+        next = NULL;
+    }
+};*/
+
+class Solution {
+  public:
+    Node* constructDLL(vector<int>& arr)
+    {
+        Node* head = new Node(arr[0]);
+        
+        Node* p = head;
+        if(arr.size() == 1)
+        {
+            return head;
+        }
+        
+        for(int i = 1; i < arr.size(); i++)
+        {
+            Node* ptr = new Node(arr[i]);
+            p -> next = ptr;
+            ptr -> prev = p;
+            p = p -> next;
+        }
+        
+        return head;
+    }
+};
