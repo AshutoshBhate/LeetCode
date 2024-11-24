1f9a619754332be5113f1f351f23750f366137c4
CeLViron <ashutoshbhate7@gmail.com>
2024-11-25 01:58:20 +0530

    NC

---
+++ 144_BinaryTreePreorderTraversal_BinaryTrees_Easy.cpp
@@ -0,0 +1,40 @@
+/*
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+
+class Solution {
+public:
+    vector<int> answer;
+
+    vector<int> preorderTraversal(TreeNode* root)
+    {
+        // if(root == NULL)
+        // {
+        //     return answer;
+        // }
+        //                                       My method, but not used
+        // answer.push_back(root -> val);
+        // preorderTraversal(root -> left);
+        // preorderTraversal(root -> right);
+
+        // return answer;
+
+
+        if(root != NULL)
+        {
+            answer.push_back(root -> val);
+            preorderTraversal(root -> left);
+            preorderTraversal(root -> right);
+        }    
+
+        return answer;      //TC: O(N) , SC: O(N) Auxiliary Space used in recursion
+    }
+};
