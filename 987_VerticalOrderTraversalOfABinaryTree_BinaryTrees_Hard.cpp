dc3bb2919c8eb7fa31cf8ee219a1271fedd0a743
CeLViron <ashutoshbhate7@gmail.com>
2024-12-02 02:53:04 +0530

    NC

--- 987_VerticalOrderTraversalOfABinaryTree_BinaryTrees_Hard.cpp
+++ 987_VerticalOrderTraversalOfABinaryTree_BinaryTrees_Hard.cpp
@@ -120,5 +120,92 @@ int main()
 
     return 0;
 }
+
+
+
+// LeetCode Solution:
+
+/*
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+    vector<vector<int>> verticalTraversal(TreeNode* root)
+    {
+        // Map to store nodes based on vertical and level information
+        map<int, map<int, multiset<int>>> nodes;
+        
+        // Queue for BFS (LevelOrder) traversal, each element is a pair containing node and its vertical and level information
+        queue<pair<TreeNode*, pair<int, int>>> todo;
+        
+        // Push the root node with initial vertica and level values (0, 0)
+        todo.push({root, {0, 0}});
+        
+        // LevelOrder
+        while(!todo.empty())
+        {
+            // Retrieve the node and its vertical and level information from the front of the queue
+            auto p = todo.front();
+            todo.pop();
+            TreeNode* temp = p.first;
+            
+            // Extract the vertical and level information
+            // x -> vertical
+            int x = p.second.first;  
+            // y -> level
+            int y = p.second.second; 
+            
+            // Insert the node value into the corresponding vertical and level in the map
+            nodes[x][y].insert(temp->val);
+            
+            // Process left child
+            if(temp->left)
+            {
+                todo.push({temp->left, {
+                        // Move left in terms of vertical
+                        x-1, 
+                        // Move down in terms of level
+                        y+1  
+                    }
+                });
+            }
+            
+            // Process right child
+            if(temp->right)
+            {
+                todo.push({temp->right, {
+                        // Move right in terms of vertical
+                        x+1, 
+                        // Move down in terms of level
+                        y+1  
+                    }
+                });
+            }
+        }
+        
+        // Prepare the final result vector by combining values from the map
+        vector<vector<int>> ans;
+        for(auto p: nodes)
+        {
+            vector<int> col;
+            for(auto q: p.second)
+            {
+                // Insert node values into the column vector
+                col.insert(col.end(), q.second.begin(), q.second.end());  //For each vertical traversal (-2, -1, 0, ...), what all nodes are there at every level
+            }
+            // Add the column vector to the final result
+            ans.push_back(col);
+        }
+        return ans;
+    }
+};
                             
                         
