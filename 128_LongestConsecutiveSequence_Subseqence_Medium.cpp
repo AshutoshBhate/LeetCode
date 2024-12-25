963e937077e93d7166cbc24dabae3f071eb148ae
CeLViron <ashutoshbhate7@gmail.com>
2024-12-25 20:02:07 +0530

    NC

--- 128_LongestConsecutiveSequence_Subseqence_Medium.cpp
+++ 128_LongestConsecutiveSequence_Subseqence_Medium.cpp
@@ -1,3 +1,5 @@
+//My Solution
+
 class Solution {
 public:
     int longestConsecutive(vector<int>& nums)
@@ -45,3 +47,50 @@ public:
         }
     }
 };
+
+//Striver Solution
+
+class Solution {
+public:
+    int longestConsecutive(vector<int>& nums)
+    {
+        int n = nums.size();
+
+        if (n == 0)
+        {
+            return 0;
+        }
+
+        int longest = 1;
+
+        unordered_set<int> st;
+
+        // put all the array elements into set:
+        for (int i = 0; i < n; i++)
+        {
+            st.insert(nums[i]);
+        }
+
+        // Find the longest sequence:
+        for (auto it : st)
+        {
+            // if 'it' is a starting number:
+            if (st.find(it - 1) == st.end()) // Checking whether 'it' has any previous element
+            {
+                // find consecutive numbers:
+                int count = 1;
+                int x = it;
+                while (st.find(x + 1) != st.end())
+                {
+                    x = x + 1;
+                    count = count + 1;
+                }
+
+                longest = max(longest, count);
+            }
+        }
+
+        return longest;
+    }
+};
+
