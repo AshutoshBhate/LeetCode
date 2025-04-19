b79395ed66ce2b8ae3af29fb130b34645404880a
CeLViron <ashutoshbhate7@gmail.com>
2025-04-19 19:11:43 +0530

    NC

--- DynamicProgramming_GeeksTraining_GFG.cpp
+++ DynamicProgramming_GeeksTraining_GFG.cpp
@@ -2,7 +2,6 @@
 
 class Solution {
   public:
-  
     int helper(int day, int last, vector<vector<int>>& arr, vector<vector<int>>& dp)
     {
         if(dp[day][last] != -1)
@@ -56,40 +55,39 @@ class Solution {
 //Declare the similar size dp array
 
 class Solution {
-    public:
-      int maximumPoints(vector<vector<int>>& arr)
-      {
-          int n = arr.size();
-          
-          vector<vector<int>> dp(n, vector<int>(3, 0));
-          
-          dp[0][0] = max(arr[0][1], arr[0][2]);   //Note that the [][0] in dp is the previous task denatation
-          dp[0][1] = max(arr[0][0], arr[0][2]);   //similar to the last of the memoization function call
-          dp[0][2] = max(arr[0][0], arr[0][1]);
-          
-          for(int day = 1; day < n; day++)
-          {   
-              for(int last = 0; last < 3; last++)
-              {
-                  for(int task = 0; task < 3; task++)
-                  {
-                      if(task != last)
-                      {
-                          int point = arr[day][task] + dp[day - 1][task];
-                          dp[day][last] = max(dp[day][last], point);
-                      }
-                  }
-              }
-          }
-          
-          int last_max = 0;
-          for(int i = 0; i < 3; i++)
-          {
-              last_max = max(last_max, dp[n-1][i]);
-          }
-          
-          return last_max;
-      }
+  public:
+    int maximumPoints(vector<vector<int>>& arr)
+    {
+        int n = arr.size();
+      
+        vector<vector<int>> dp(n, vector<int>(4, 0));
+      
+        dp[0][0] = max(arr[0][1], arr[0][2]);   //Note that the [][0] in dp is the previous task denatation
+        dp[0][1] = max(arr[0][0], arr[0][2]);   //similar to the last of the memoization function call
+        dp[0][2] = max(arr[0][0], arr[0][1]);
+      
+        for(int day = 1; day < n; day++)
+        {   
+            for(int last = 0; last < 4; last++)
+            {
+                for(int task = 0; task < 3; task++)
+                {
+                    if(task != last)
+                    {
+                        int point = arr[day][task] + dp[day - 1][task];
+                        dp[day][last] = max(dp[day][last], point);
+                    }
+                }
+            }
+        }
+        int last_max = 0;
+        for(int i = 0; i < 4; i++)
+        {
+            last_max = max(last_max, dp[n-1][i]);
+        }
+
+        return last_max;
+    }
 };
 
 //TC : O(N*4*3)
@@ -98,43 +96,43 @@ class Solution {
 //Space Optimisation:
 
 class Solution {
-    public:
-      int maximumPoints(vector<vector<int>>& arr)
-      {
-          int n = arr.size();
-          vector<int> prev(3, 0);
-          
-          prev[0] = max(arr[0][1], arr[0][2]);
-          prev[1] = max(arr[0][0], arr[0][2]);
-          prev[2] = max(arr[0][0], arr[0][1]);
-          
-          for(int day = 1; day < n; day++)
-          {
-              vector<int> temp(3, 0);
-              for(int last = 0; last < 4; last++)
-              {
-                  temp[last] = 0;
-                  
-                  for(int task = 0; task < 3; task++)
-                  {
-                      if(task != last)
-                      {
-                          temp[last] = max(temp[last], arr[day][task] + prev[task]);
-                      }
-                  }
-              }
-              prev = temp;
-          }
-          
-          int final_max = 0;
-          
-          for(int i = 0; i < 3; i++)
-          {
-              final_max = max(final_max,prev[i]);
-          }
-          
-          return final_max;
-      }
+  public:
+        int maximumPoints(vector<vector<int>>& arr)
+        {
+            int n = arr.size();
+            vector<int> prev(4, 0);
+        
+            prev[0] = max(arr[0][1], arr[0][2]);
+            prev[1] = max(arr[0][0], arr[0][2]);
+            prev[2] = max(arr[0][0], arr[0][1]);
+        
+            for(int day = 1; day < n; day++)
+            {
+                vector<int> temp(4, 0);
+                for(int last = 0; last < 4; last++)
+                {
+                    temp[last] = 0;
+                
+                    for(int task = 0; task < 3; task++)
+                    {
+                        if(task != last)
+                        {
+                        temp[last] = max(temp[last], arr[day][task] + prev[task]);
+                        }
+                    }
+                }
+                prev = temp;
+            }
+        
+            int final_max = 0;
+        
+            for(int i = 0; i < 4; i++)
+            {
+            final_max = max(final_max,prev[i]);
+            }
+        
+            return final_max;
+        }
 };
 
 //TC : O(N*4*3)
