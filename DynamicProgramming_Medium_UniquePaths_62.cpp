35fc5ad5b6eb5aa27b28a194ea20fe83bbf5d334
CeLViron <ashutoshbhate7@gmail.com>
2025-04-20 16:46:40 +0530

    NC

--- DynamicProgramming_Medium_UniquePaths_62.cpp
+++ DynamicProgramming_Medium_UniquePaths_62.cpp
@@ -18,10 +18,10 @@ class Solution {
                 return dp[m][n];
             }    
     
-            int down = helper(m - 1, n, dp);
-            int right = helper(m, n - 1, dp);
+            int up = helper(m - 1, n, dp);
+            int left = helper(m, n - 1, dp);
     
-            dp[m][n] = down + right;
+            dp[m][n] = up + left;
             return dp[m][n];
         }
     
@@ -39,7 +39,102 @@ class Solution {
 
 //Tabulation : 
 
-// To remove the recurssion stack space
+//To remove the recursion stack space
+//In tabulation, first whatever is the base case in recursion, write that
+//Also declare a dp array of size m and n
+
+//Express whatever you can in dp. For eg. dp[0][0] = 1
+
+//In tabulation, you start from the base case
+
+class Solution {
+    public:
+    
+        int uniquePaths(int m, int n)
+        {
+            vector<vector<long long>> dp(m, vector<long long>(n, 0));
+    
+            for(int i = 0; i < m; i++)
+            {
+                for(int j = 0; j < n; j++)
+                {
+                    long long up = 0;
+                    long long left = 0;
+    
+                    if(i == 0 && j == 0)
+                    {
+                        dp[0][0] = 1;
+                    }
+                    else
+                    {
+                        if(i > 0)
+                        {
+                            up = dp[i - 1][j];
+                        }
+                        if(j > 0)
+                        {
+                            left = dp[i][j - 1];
+                        }
+    
+                        dp[i][j] = up + left;
+                    }
+                }
+            }
+    
+            return dp[m - 1][n - 1];
+        }
+};
+
+//TC: O(N*M)
+//SC: O(N*M)
+
+//Space Optimisation :
+
+class Solution {
+    public:
+        int uniquePaths(int m, int n)
+        {
+            vector<long long> prev(n, 0);
+    
+            for(int i = 0; i < m; i++)
+            {
+                vector<long long> curr(n, 0);
+                for(int j = 0; j < n; j++)
+                {
+                    if(i == 0 && j == 0)
+                    {
+                        curr[j] = 1;
+                    }
+                    else
+                    {
+                        long long up = 0;
+                        long long left = 0;
+    
+                        if(i > 0)
+                        {
+                            up = prev[j];
+                        }
+                        if(j > 0)
+                        {
+                            left = curr[j - 1];
+                        }
+    
+                        curr[j] = up + left;
+                    }
+                }
+    
+                prev = curr;
+            }
+    
+            return prev[n - 1];
+        }
+};
+
+//TC: O(N*M)
+//SC: O(N)
+
+
+
 
 
 
