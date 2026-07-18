59c3d1f83b290922e5670b287ac48ca2318f7b7b
CeLViron <ashutoshbhate7@gmail.com>
2026-07-18 12:55:21 +0530

    NC

--- C++/Array_CountInversions_GFG.cpp
+++ C++/Array_CountInversions_GFG.cpp
@@ -1,64 +1,75 @@
-#include <vector>
-
-using namespace std;
-
 class Solution {
-public:
-    void merge(vector<int>& arr, int low, int mid, int high, int& count_inversions)
+  public:
+  
+    int mergeHalves(vector<int>& arr, int low, int mid, int high)
     {
-        vector<int> temp;
-
         int left = low;
         int right = mid + 1;
-
-        while (left <= mid && right <= high)
+        int count = 0;
+        vector<int> temp;
+        
+        while(left <= mid && right <= high)
         {
-            if (arr[left] <= arr[right])
+            if(arr[left] > arr[right])
+            {
+                count += (mid - left + 1);
+                temp.push_back(arr[right]);
+                right++;
+            }
+            else
             {
                 temp.push_back(arr[left]);
                 left++;
             }
-            else
-            {
-                count_inversions += mid - left + 1;
-                temp.push_back(arr[right]);
-                right++;
-            }
         }
-
-        while (left <= mid)
+        
+        while(left <= mid)
         {
             temp.push_back(arr[left]);
             left++;
         }
-        while (right <= high)
+        while(right <= high)
         {
             temp.push_back(arr[right]);
             right++;
         }
-
-        for (int i = low; i <= high; i++)
+        
+        for(int i = low; i <= high; i++)
         {
             arr[i] = temp[i - low];
         }
+        
+        return count;
     }
-
-    void MS(vector<int>& arr, int low, int high, int& count_inversions)
+    
+    int mergeSort(vector<int>& arr, int low, int high)
     {
-        if (low >= high) return;
-
-        int mid = (low + high) / 2;
-
-        MS(arr, low, mid, count_inversions);
-        MS(arr, mid + 1, high, count_inversions);
-
-        merge(arr, low, mid, high, count_inversions);
+        int count = 0;
+        
+        if(low >= high)
+        {
+            return 0;
+        }
+        
+        int mid = int((low + high)/2);
+        
+        count += mergeSort(arr, low, mid);
+        count += mergeSort(arr, mid+1, high);
+        count += mergeHalves(arr, low, mid, high);
+        
+        return count;
+        
     }
-
-    int inversionCount(vector<int>& arr)
+  
+    int inversionCount(vector<int> &arr)
     {
-        int count_inversions = 0;
-        MS(arr, 0, arr.size() - 1, count_inversions);
-        return count_inversions;
+        int count = 0;
+        int low = 0;
+        int high = arr.size() - 1;
+        
+        count = mergeSort(arr, low, high);
+        
+        return count;
+        
     }
 };
