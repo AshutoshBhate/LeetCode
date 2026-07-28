8dc7dcf1578d030f58f1d4d12365fcb0c2f08d60
CeLViron <ashutoshbhate7@gmail.com>
2026-07-28 12:13:09 +0530

    NC

--- C++/LinkedList_Medium_SortList_148.cpp
+++ C++/LinkedList_Medium_SortList_148.cpp
@@ -1,3 +1,54 @@
+//Brute-Force Approach : 
+
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode() : val(0), next(nullptr) {}
+ *     ListNode(int x) : val(x), next(nullptr) {}
+ *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+ * };
+ */
+class Solution {
+public:
+    ListNode* sortList(ListNode* head) 
+    {
+        vector<int> vec_1;
+        ListNode* temp = head;
+        ListNode* prev;
+        ListNode* start;
+
+        while(temp != NULL)
+        {
+            vec_1.push_back(temp -> val);
+            temp = temp -> next;
+        }
+
+        sort(vec_1.begin(), vec_1.end());
+
+        if(vec_1.size() == 0)
+        {
+            return NULL;
+        }
+        else
+        {
+            prev = new ListNode(vec_1[0]);
+            start = prev;
+        }
+        for(int i = 1; i < vec_1.size(); i++)
+        {
+            ListNode* temp = new ListNode(vec_1[i]);
+            prev -> next = temp;
+            prev = temp;
+        }
+        
+        return start;
+    }
+};
+
+//Optimal Approach : 
+
 /*
  * Definition for singly-linked list.
  * struct ListNode {
