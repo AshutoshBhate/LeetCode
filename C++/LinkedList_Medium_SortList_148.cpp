6287bf2887de0e5db9ee15bcf6a9f6012dc16824
CeLViron <ashutoshbhate7@gmail.com>
2026-07-31 20:19:55 +0530

    NC

--- C++/LinkedList_Medium_SortList_148.cpp
+++ C++/LinkedList_Medium_SortList_148.cpp
@@ -47,9 +47,7 @@ public:
     }
 };
 
-//Optimal Approach : 
-
-/*
+/**
  * Definition for singly-linked list.
  * struct ListNode {
  *     int val;
@@ -59,99 +57,72 @@ public:
  *     ListNode(int x, ListNode *next) : val(x), next(next) {}
  * };
  */
-
 class Solution {
 public:
-    ListNode* mergeTwoSortedLinkedLists(ListNode* list1, ListNode* list2)
-    {
-        // Create a dummy node to serve
-        // as the head of the merged list
-        ListNode* dummyNode = new ListNode(-1);
-        ListNode* temp = dummyNode;
 
-        // Traverse both lists simultaneously
-        while (list1 != nullptr && list2 != nullptr)
-        {
-            // Compare elements of both lists and
-            // link the smaller node to the merged list
-            if (list1->val <= list2->val)
-            {
-                temp->next = list1;
-                list1 = list1->next;
-            }
-            else
-            {
-                temp->next = list2;
-                list2 = list2->next;
-            }
-            // Move the temporary pointer
-            // to the next node
-            temp = temp->next;
-        }
-
-        // If any list still has remaining
-        // elements, append them to the merged list
-        if(list1 != nullptr)
-        {
-            temp -> next = list1;
-        }
-       else
-        {
-            temp -> next = list2;
-        }
-        // Return the merged list starting
-        // from the next of the dummy node
-        return dummyNode->next;
-    }
-
-    // Function to find the middle of a linked list
     ListNode* findMiddle(ListNode* head)
     {
-        // If the list is empty or has only one node
-        // the middle is the head itself
-        if (head == nullptr || head->next == nullptr)
-        {
-            return head;
-        }
-
-        // Initializing slow and fast pointers
         ListNode* slow = head;
-        ListNode* fast = head->next;
+        ListNode* fast = head -> next;
 
-        // Move the fast pointer twice as fast as the slow pointer
-        // When the fast pointer reaches the end, the slow pointer
-        // will be at the middle
-        while (fast != nullptr && fast->next != nullptr)
+        while(fast != NULL && fast -> next != NULL)
         {
-            slow = slow->next;
-            fast = fast->next->next;
+            slow = slow -> next;
+            fast = fast -> next -> next;
         }
 
         return slow;
     }
 
-    ListNode* sortList(ListNode* head)
+    ListNode* mergeHalves(ListNode* leftNode, ListNode* rightNode)
     {
-        // Base case: if the list is empty or has only one node
-        // it is already sorted, so return the head
-        if (head == nullptr || head->next == nullptr)
+        ListNode* ptr = new ListNode();
+        ListNode* temp = ptr;
+        while(leftNode != NULL && rightNode != NULL)
+        {
+            if(leftNode -> val < rightNode -> val)
+            {
+                temp -> next = leftNode;
+                temp = temp -> next;
+                leftNode = leftNode -> next;
+            }
+            else
+            {
+                temp -> next = rightNode;
+                temp = temp -> next;
+                rightNode = rightNode -> next;
+            }
+        }
+        if(leftNode != NULL)
+        {
+            temp -> next = leftNode;
+        }
+        else
+        {
+            temp -> next = rightNode;
+        }
+        
+        ListNode* result = ptr -> next;
+        delete ptr;
+        return result;
+    }
+
+    ListNode* sortList(ListNode* head) 
+    {
+        if(head == NULL || head -> next == NULL)
         {
             return head;
         }
 
-        // Find the middle of the list using the findMiddle function
         ListNode* middle = findMiddle(head);
-
-        // Divide the list into two halves
-        ListNode* right = middle->next;
-        middle->next = nullptr;
         ListNode* left = head;
+        ListNode* right = middle -> next;
 
-        // Recursively sort the left and right halves
-        left = sortList(left);
-        right = sortList(right);
+        middle -> next = NULL;
 
-        // Merge the sorted halves using the mergeTwoSortedLinkedLists function
-        return mergeTwoSortedLinkedLists(left, right);
+        ListNode* leftNode = sortList(left);
+        ListNode* rightNode = sortList(right);
+
+        return mergeHalves(leftNode, rightNode);
     }
 };
