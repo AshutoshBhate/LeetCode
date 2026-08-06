0e6e4f6c7d8c062c30d70ac6e53bb2572d992da4
CeLViron <ashutoshbhate7@gmail.com>
2026-08-07 00:30:57 +0530

    NC

--- Python/LinkedList_Medium_RemoveNthNodeFromEndOfList_19.py
+++ Python/LinkedList_Medium_RemoveNthNodeFromEndOfList_19.py
@@ -38,6 +38,7 @@ class Solution:
 #     def __init__(self, val=0, next=None):
 #         self.val = val
 #         self.next = next
+
 class Solution:
     def removeNthFromEnd(self, head: Optional[ListNode], n: int) -> Optional[ListNode]:
         fast = head
