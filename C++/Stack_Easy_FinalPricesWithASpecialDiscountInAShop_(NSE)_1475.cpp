29c65f0d88c1e259ddd8dcc152694e62dee6eeb8
CeLViron <ashutoshbhate7@gmail.com>
2026-08-12 00:12:51 +0530

    NC

---
+++ C++/Stack_Easy_FinalPricesWithASpecialDiscountInAShop_(NSE)_1475.cpp
@@ -0,0 +1,48 @@
+/*
+[NGE] (Already completed)
+  │
+  ▼
+Phase 1: Next Smaller Element (NSE) (Doing it in this code)  ──► Keeps direction (right), flips operator
+  │
+  ▼
+Phase 2: Previous Greater Element (PGE)  ──► Keeps condition (greater), flips direction (left)
+  │
+  ▼
+Phase 3: Previous Smaller Element (PSE)  ──► Flips both (left + smaller)
+  │
+  ▼
+Phase 4: Combined Bounds (NSE + PSE)  ──► The interview standard (Histogram & Subarrays)
+*/
+
+class Solution {
+public:
+    vector<int> finalPrices(vector<int>& prices) 
+    {
+        cin.tie(nullptr);
+        cout.tie(nullptr);
+        ios::sync_with_stdio(false);
+
+        vector<int> answers(prices.size(), 0);
+        stack<int> myStack;
+
+        for(int i = prices.size() - 1; i >= 0; i--)
+        {
+            while(!myStack.empty() && prices[i] < myStack.top())
+            {
+                myStack.pop();
+            }
+
+            if(myStack.empty() == true)
+            {
+                myStack.push(prices[i]);
+                answers[i] = prices[i];
+                continue;
+            }
+            
+            answers[i] = prices[i] - myStack.top();
+            myStack.push(prices[i]);
+        }  
+
+        return answers;
+    }
+};
